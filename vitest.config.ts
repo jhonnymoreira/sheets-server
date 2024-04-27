36b0fe3d6430a3f625a5cf66db2475250eacfbc0
Jhonny Moreira <jhonnymoreira@protonmail.com>
2024-04-26 21:33:32 -0300

    chore: support test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    coverage: {
+      include: ['src/**/*.ts'],
+    },
     globals: true,
     watch: false,
   },
