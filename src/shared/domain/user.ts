f21145c1275d00df76a1cb04acfbe9543f5e4682
Jhonny Moreira <jhonnymoreira@protonmail.com>
2024-05-01 04:12:25 -0300

    chore: loosen the strictness upon domain insertion

--- src/shared/domain/user.ts
+++ src/shared/domain/user.ts
@@ -1,12 +1,9 @@
 import { createInsertSchema } from 'drizzle-zod';
-import { nanoid } from 'nanoid';
 import { z } from 'zod';
-import { ID_LENGTH, users } from '@/db/schema.js';
+import { users } from '@/db/schema.js';
 
 export const insertUserSchema = createInsertSchema(users, {
   email: (schema) => schema.email.trim().email(),
-  githubHandler: (schema) => schema.githubHandler.trim().min(1),
-  id: z.string().trim().length(ID_LENGTH).optional().default(nanoid(ID_LENGTH)),
 });
 
 export type User = typeof users.$inferSelect;
--- tests/shared/domain/user.test.ts
+++ tests/shared/domain/user.test.ts
@@ -1,12 +1,9 @@
-import { ID_LENGTH } from '@/db/schema.js';
 import { insertUserSchema } from '@/shared/domain/index.js';
 
 const {
   shape: {
     createdAt: createdAtSchema,
     email: emailSchema,
-    githubHandler: githubHandlerSchema,
-    id: idSchema,
     updatedAt: updatedAtSchema,
   },
 } = insertUserSchema;
@@ -51,102 +48,6 @@ describe('User Domain', () => {
             }
           ]]
         `);
-      });
-    });
-
-    describe('githubHandler', () => {
-      test('should be a string', () => {
-        const { error } = githubHandlerSchema.safeParse(123);
-
-        expect(error).toMatchInlineSnapshot(`
-          [ZodError: [
-            {
-              "code": "invalid_type",
-              "expected": "string",
-              "received": "number",
-              "path": [],
-              "message": "Expected string, received number"
-            }
-          ]]
-        `);
-      });
-
-      test('should have at least 1 character', () => {
-        const { error } = githubHandlerSchema.safeParse('');
-
-        expect(error).toMatchInlineSnapshot(`
-          [ZodError: [
-            {
-              "code": "too_small",
-              "minimum": 1,
-              "type": "string",
-              "inclusive": true,
-              "exact": false,
-              "message": "String must contain at least 1 character(s)",
-              "path": []
-            }
-          ]]
-        `);
-      });
-
-      test('should not exceed 39 characters', () => {
-        const { error } = githubHandlerSchema.safeParse('a'.repeat(39 + 1));
-
-        expect(error).toMatchInlineSnapshot(`
-          [ZodError: [
-            {
-              "code": "too_big",
-              "maximum": 39,
-              "type": "string",
-              "inclusive": true,
-              "exact": false,
-              "message": "String must contain at most 39 character(s)",
-              "path": []
-            }
-          ]]
-        `);
-      });
-    });
-
-    describe('id', () => {
-      test('should be a string', () => {
-        const { error } = idSchema.safeParse(123);
-
-        expect(error).toMatchInlineSnapshot(`
-            [ZodError: [
-              {
-                "code": "invalid_type",
-                "expected": "string",
-                "received": "number",
-                "path": [],
-                "message": "Expected string, received number"
-              }
-            ]]
-          `);
-      });
-
-      test('should be a non-empty string with a fixed length matching the ID_LENGTH', () => {
-        const { error } = idSchema.safeParse('       ');
-
-        expect(error).toMatchInlineSnapshot(`
-            [ZodError: [
-              {
-                "code": "too_small",
-                "minimum": 21,
-                "type": "string",
-                "inclusive": true,
-                "exact": true,
-                "message": "String must contain exactly 21 character(s)",
-                "path": []
-              }
-            ]]
-          `);
-      });
-
-      test('defaults to a random ID of length matching the ID_LENGTH', () => {
-        const { data } = idSchema.safeParse(undefined);
-
-        expect(data).toHaveLength(ID_LENGTH);
       });
     });
 
